a628cac692a00547e91de7071910c5651c2e3ecb
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-04-07 01:45:17 +0200

    Add basic code to Navbar component

--- src/app/Common/Components/navbar/navbar.component.ts
+++ src/app/Common/Components/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Route {
+  title: string;
+  href: string;
+  active: boolean;
+}
 
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent implements OnInit {
+  posibleRoutes: Route[];
+  constructor(private activeRoute: Router) { 
+    console.log(this.activeRoute.isActive('login', false));
+    this.posibleRoutes  = [
+      {href: '/login', title: 'Login', active: false},{href: '/register', title:'Register', active: true}
+    ];
+   }
 
   ngOnInit(): void {
+
   }
 
 }
