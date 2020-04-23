882062a67b669720361759b96b4a2bf60933692a
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-04-23 23:55:12 +0200

    Add basic logic to user-list component

--- src/app/Admin/user-list/user-list.component.ts
+++ src/app/Admin/user-list/user-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Common/Types/user';
+import { HttpClient } from '@angular/common/http';
+import { Farmer } from 'src/app/Common/Types/farmer';
+import { Company } from 'src/app/Common/Types/company';
+import { Admin } from 'src/app/Common/Types/admin';
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
-  constructor() { }
+  farmerList: Farmer[] = [];
+  companyList: Company[] = [];
+  adminList: Admin[] = [];
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/users')
+      .subscribe((data: User[]) => {
+        console.log(data);
+        data.forEach((user: User) => {
+          if (user.role_id == 1) {
+            this.adminList.push(User.toAdmin(user));
+          }
+          if (user.role_id == 2) {
+            this.companyList.push(User.toCompany(user))
+          } 
+          if (user.role_id == 3) {
+            this.farmerList.push(User.toFarmer(user));
+          }
+        });
+      });
+  }
+
+  deleteUser(id: number) {
+    console.log(id);
+  }
+
+  editUser(id: number) {
+    console.log(id);
   }
 
 }
