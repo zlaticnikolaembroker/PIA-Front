360527808411823643aeda12f5dcea9ef06a8cc3
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-04-25 02:07:59 +0200

    Add initial code to Admin component

--- src/app/Admin/Components/admin/admin.component.ts
+++ src/app/Admin/Components/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Admin } from 'src/app/Common/Types/admin';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -9,11 +10,61 @@ import { Admin } from 'src/app/Common/Types/admin';
 export class AdminComponent implements OnInit {
   @Input() user: Admin;
   @Input() update: Function;
-  constructor() { }
+
+  userEdited: boolean;
+  passwordNotStrong: boolean;
+
+  passRegex = new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})");
+
+  initFlags(){
+    this.userEdited = false;
+    this.passwordNotStrong = false;
+  }
+
+  constructor(private router: Router) { 
+  }
+
+  tempUser: Admin;
 
   ngOnInit(): void {
-    console.log(this.user);
+    this.tempUser = this.user;
+  }
+
+  checkIsUserEdited() {
+    if (this.user.email === this.tempUser.email && this.user.password === this.tempUser.password && this.user.username === this.tempUser.username) {
+      this.userEdited = true;
+      return false;
+    }
+    return true;
+  }
+
+  checkIsEmailFormatOK(){
+    if (!this.passRegex.test(this.tempUser.password)) {
+      this.passwordNotStrong = true;
+      return false;
+    }
+    return true;
+  }
+
+  checkData(){
+    if (!this.checkIsUserEdited()) {
+      return false;
+    }
+    if (!this.checkIsEmailFormatOK()) {
+      return false;
+    }
+    return true;
+  }
+  handleUpdate() {
+    if (!this.checkData()){
+      return;
+    }
     this.update(this.user);
+    this.update(this.tempUser);
+  }
+
+  back() {
+    this.router.navigate(['/admin/user-list'])
   }
 
 }
