5b677dabd19a0e369f67f2ff4d38d94e012dd1f5
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-04-30 01:58:17 +0200

    Add api call to Product component

--- src/app/Company/Components/product/product.component.ts
+++ src/app/Company/Components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductDetails } from '../../types/ProductDetails';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    this.http.get('http://localhost:3000/company/get_product_details/2')
+    .subscribe((data: ProductDetails) => {
+          console.log(data);
+          });
+    }
 
 }
