049857f45ea64648774079ab642d1669426dc128
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-01 15:11:58 +0200

    Add data check before update

--- src/app/Company/Components/product/product.component.ts
+++ src/app/Company/Components/product/product.component.ts
@@ -10,7 +10,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductComponent implements OnInit {
   productDetails: ProductDetails;
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  message: string;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { 
+    this.message = '';
+  }
   ngOnInit(): void {
     this.route.params.subscribe( params => {
       if (params['productId']) {
@@ -23,6 +27,36 @@ export class ProductComponent implements OnInit {
         });
       }
     });
+  }
+
+  checkDataCorrectness(): boolean {
+    if (this.productDetails.available < 0) {
+      this.message = 'Value for available can\'t be less than zero.'
+      return false;
+    } 
+    if (+this.productDetails.price === NaN) {
+      this.message = 'Value for price have to be a valid number.'
+      return false;
     }
+    return true;
+  }
+
+  checkValues(): boolean {
+    if (this.productDetails.available === null || this.productDetails.available === undefined ||
+      this.productDetails.price === null || this.productDetails.price === undefined ||
+      this.productDetails.name === null || this.productDetails.name === undefined || this.productDetails.name === ''
+      ) {
+      this.message = 'Please fill out all fields.';
+      return false;
+    }
+
+    return true;
+  }
+
+  handleUpdate() {
+    this.message = '';
+    this.checkValues();
+    this.checkDataCorrectness();
+  }
 
 }
