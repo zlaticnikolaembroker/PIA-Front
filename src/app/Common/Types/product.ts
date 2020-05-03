07fef6f6e9f9afac1fde7d9fd7dfa97b49089e78
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-03 22:18:03 +0200

    Add optional field archived to product type

--- src/app/Common/Types/product.ts
+++ src/app/Common/Types/product.ts
@@ -1,7 +1,10 @@
+import { Nullable } from './nullable';
+
 export interface Product {
     id: number;
     name: string;
     price: number;
     available: number;
     company_id: number;
+    archived: Nullable<boolean>;
 }
