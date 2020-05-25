ed930ac5d0c1cadcb3fc882b47fe86335ae39ba5
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-25 23:31:51 +0200

    Add Garden type and table of gardens to farmer dashboard

--- src/app/Farmer/Components/farmer-dashboard/farmer-dashboard.component.ts
+++ src/app/Farmer/Components/farmer-dashboard/farmer-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Garden } from '../../types/Garden';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-farmer-dashboard',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmerDashboardComponent implements OnInit {
 
-  constructor() { }
+  gardens: Garden[];
+  constructor(private http: HttpClient, private cookieService: CookieService, private router: Router) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/farmer/gardens/' + this.cookieService.get("userId")).subscribe((data: Garden[]) => {
+          this.gardens = data;
+      });
+  }
+
+  detailsClicked(id) {
+    console.log(id);
+  }
+
+  warehouseClicked(id) {
+    console.log(id);
   }
 
 }
---
+++ src/app/Farmer/types/Garden.ts
@@ -0,0 +1,9 @@
+export interface Garden {
+    id: number, 
+    name: string,
+    place: string,
+    free_slots: number,
+    occupied_slots: number,
+    water: number,
+    temperature: number,
+}
