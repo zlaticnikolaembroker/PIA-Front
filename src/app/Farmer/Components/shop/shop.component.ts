6ae6423ed3fe5ea88c4f224b2fe6fed9b0ab55c8
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-28 03:03:24 +0200

    Add sorting logic to products table on Shop page

--- src/app/Farmer/Components/shop/shop.component.ts
+++ src/app/Farmer/Components/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
-
+  private lastSortingParameter: string = '';
+  constructor(private http: HttpClient) { }
+  products;
   ngOnInit(): void {
+    this.http.get('http://localhost:3000/farmer/online_shop').subscribe((data) => {
+          this.products = data;
+      });
+  }
+
+  sort(field) {
+    this.products = this.products.sort((a, b) => {
+      if (!a[field]) {
+        return this.lastSortingParameter == field ? 1 : -1;
+      }
+      if (!b[field]) {
+        return this.lastSortingParameter == field ? -1 : 1;
+      }
+      if (a[field] > b[field]) {
+        return this.lastSortingParameter == field ? -1 : 1;
+      }
+      if (a[field] < b[field]) {
+        return this.lastSortingParameter == field ? 1 : -1;
+      }
+      return 0;
+    });
+    this.lastSortingParameter =  this.lastSortingParameter == field ? null : field;
   }
 
 }
