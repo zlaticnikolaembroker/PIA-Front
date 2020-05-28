41665cdfb067871fa8950fd95cda7d958ed68da6
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-28 02:10:02 +0200

    Implement "Gardens to maintenance" logic

--- src/app/Farmer/Components/farmer-dashboard/farmer-dashboard.component.ts
+++ src/app/Farmer/Components/farmer-dashboard/farmer-dashboard.component.ts
@@ -11,12 +11,16 @@ import { Router } from '@angular/router';
 })
 export class FarmerDashboardComponent implements OnInit {
 
+  gardensToMaintenance: Garden[];
   gardens: Garden[];
   constructor(private http: HttpClient, private cookieService: CookieService, private router: Router) { }
 
   ngOnInit(): void {
     this.http.get('http://localhost:3000/farmer/gardens/' + this.cookieService.get("userId")).subscribe((data: Garden[]) => {
           this.gardens = data;
+          this.gardensToMaintenance = this.gardens.filter((garden) => {
+            return garden.temperature < 12 || garden.water < 75;
+          })
       });
   }
 
