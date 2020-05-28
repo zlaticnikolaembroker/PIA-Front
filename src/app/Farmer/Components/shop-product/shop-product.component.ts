f174d96b35a7297038271c081c47e937a9a73f98
ZlaticNikolaEmbroker <nikola.zlatic@embroker.com>
2020-05-28 20:48:48 +0200

    Create UI and basic logic for Shop-Product component

--- src/app/Farmer/Components/shop-product/shop-product.component.ts
+++ src/app/Farmer/Components/shop-product/shop-product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop-product',
@@ -10,18 +10,43 @@ import { ActivatedRoute } from '@angular/router';
 export class ShopProductComponent implements OnInit {
 
   product;
+  amountOfOrder;
+  message = null;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.amountOfOrder = 0;
     this.route.params.subscribe( params => {
       if(params['productId']) {
         this.http.get('http://localhost:3000/farmer/online_shop/' + params['productId']).subscribe((data) => {
           this.product = data;
-          console.log(this.product);
         });
       }
     });
+  }
+
+  handleBackClicked() {
+    this.router.navigate(['/farmer/online_shop/'])
+  }
+
+  checkOrder():boolean {
+    if (this.amountOfOrder <= 0) {
+      this.message = "Please enter number greater than zero.";
+      return false;
+    }
+    if (this.amountOfOrder > this.product.available) {
+      this.message = "Not enough available products.";
+      return false;
+    }
+    return true;
+  }
+
+  handleOrderClicked() {
+    this.message = null;
+    if (this.checkOrder()){
+
+    }
   }
 
 }
